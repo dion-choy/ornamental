484bf36530947eb326860132bff27103ef5ad42f
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2024-12-18 07:29:19 +0800

    ui improvement and motion package installation
    note to self: make sure other css transitions are not conflicting with framer motion transition.

--- frontend/ornamental-frontend/src/app/home/page.jsx
+++ frontend/ornamental-frontend/src/app/home/page.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import css from "@/styles/home.css";
+import { motion } from "motion/react"
 
 function Home() {
+
+  const [snowPos, setSnowPos] = useState([])
+  const [displayInputCode, setDisplayInputCode] = useState(false);
+
+  function createSnowflake() {
+    return ({
+      id: Math.random().toString(36).substr(2, 9),
+      leftOffset: Math.random() * window.innerWidth,
+      animationDelay: Math.random() * 5
+    });
+  }
+
+  function replaceSnowPos(id) {
+    setSnowPos((prevSnow) =>
+      prevSnow.map((flake) =>
+        flake.id === id ? createSnowflake() : flake
+      )
+    );
+  }
+
+  useEffect(() => {
+    const initialSnow = Array.from({ length: 10 }, createSnowflake);
+    setSnowPos(initialSnow);
+  }, []);
+
+
   return (
     <div>
       <div className="container">
@@ -9,29 +37,39 @@ function Home() {
           <img src="/logo.png" alt="Ornamental Logo" />
         </div>
 
+        <p style={{fontSize: "40px"}}>It's secret santa</p>
+        <p style={{fontSize: "20px"}}>but <b>better</b></p>
+
         <div className="candy-cane">
           <img src="/candycane.png" alt="Candy Cane Divider" />
         </div>
 
         <div className="buttons">
-          <button className="btn create">Create your Room</button>
-          <button className="btn join">Join a Room</button>
-          <button className="btn code">Enter Room code</button>
+          <button className="btn create">Create a Room</button>
+          <button className="btn join" onClick={() => setDisplayInputCode(true)}>Join a Room</button>
+          
+          {displayInputCode? 
+          (<motion.input initial={{ opacity: 0, y: -50 }}
+            animate={{opacity: 1, y: 0}}
+            transition={{duration: 0.4, y: {type: "spring", visualDuration: 0.4, bounce: 0.5}}}
+            
+            placeholder="Enter Room code" id="code" className="btn"></motion.input>)
+           : null}
+          
         </div>
+
+        {snowPos.map((snowflake, index) => (
+          <div key={snowflake.id} className="snowflake" style={{ top: "-10%", left: `${snowflake.leftOffset}px`, animationDelay: `${snowflake.animationDelay}s` }} onAnimationEnd={() => replaceSnowPos(snowflake.id)}>
+            <img src="/snowflake.png" alt="Snowflake" />
+          </div>
+        ))}
       </div>
 
-      <div className="snowflake" style={{ top: "10%", left: "15%" }}>
-        <img src="/snowflake.png" alt="Snowflake" />
+      <div className="info-container">
+        <h2>About</h2>
       </div>
-      <div className="snowflake" style={{ top: "20%", right: "10%" }}>
-        <img src="/snowflake.png" alt="Snowflake" />
-      </div>
-      <div className="snowflake" style={{ bottom: "10%", left: "10%" }}>
-        <img src="/snowflake.png" alt="Snowflake" />
-      </div>
-      <div className="snowflake" style={{ bottom: "5%", right: "5%" }}>
-        <img src="/snowflake.png" alt="Snowflake" />
-      </div>
+
+
     </div>
   );
 }
