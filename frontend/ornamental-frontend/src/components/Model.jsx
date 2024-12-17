301a4f4bc2810461b86aba83b699dfb6234ed89a
dion-choy <dionchoynj@gmail.com>
2024-12-17 22:07:01 +0800

    run animations if any in Model component

--- frontend/ornamental-frontend/src/components/Model.jsx
+++ frontend/ornamental-frontend/src/components/Model.jsx
@@ -1,15 +1,18 @@
 import React, { useRef, useEffect } from "react";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { useLoader, useFrame } from "@react-three/fiber";
+import { AnimationMixer } from "three";
 
 function Model(props) {
     const gltf = useLoader(GLTFLoader, props.file);
     const rotate = useRef();
+    let mixer;
 
-    useFrame(() => {
+    useFrame((state, delta) => {
         if (props.rotate) {
             rotate.current.rotation.y += props.rotate;
         }
+        mixer?.update(delta);
     });
 
     useEffect(() => {
@@ -19,6 +22,13 @@ function Model(props) {
                 child.receiveShadow = true;
             }
         });
+
+        if (gltf.animations.length) {
+            mixer = new AnimationMixer(gltf.scene);
+            for (const anim of gltf.animations) {
+                mixer.clipAction(anim).play();
+            }
+        }
     }, [gltf]);
 
     return (
