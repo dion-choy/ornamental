01b8783e6a2887f3aa9faa50ee1ff2d648deb3e0
dion-choy <dionchoynj@gmail.com>
2024-12-18 16:23:34 +0800

    rendering

--- frontend/ornamental-frontend/src/app/page.jsx
+++ frontend/ornamental-frontend/src/app/page.jsx
@@ -4,6 +4,11 @@ import { useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import MyScene from "@/components/CanvasScene";
 
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { SAOPass } from "three/addons/postprocessing/SAOPass.js";
+import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
+
 export default function Home() {
     const cam = useRef();
     const [numReindeers, setNumReindeers] = useState(0);
@@ -16,6 +21,32 @@ export default function Home() {
                 camera={{
                     position: [7, 4, 7],
                     fov: 100,
+                }}
+                onCreated={({ gl, scene, camera }) => {
+                    const composer = new EffectComposer(gl);
+                    const renderPass = new RenderPass(scene, camera);
+                    composer.addPass(renderPass);
+
+                    const saoPass = new SAOPass(scene, camera);
+                    composer.addPass(saoPass);
+
+                    const outputPass = new OutputPass();
+                    composer.addPass(outputPass);
+                    saoPass.resolution.set(1024, 1024);
+                    saoPass.setSize(1024, 1024);
+                    console.log(saoPass);
+
+                    saoPass.params.saoBias = -1;
+                    saoPass.params.saoIntensity = 0.5;
+                    saoPass.params.saoScale = 1;
+                    saoPass.params.saoKernelRadius = 100;
+                    saoPass.params.saoMinResolution = 0.01;
+                    saoPass.params.saoBlur = true;
+                    saoPass.params.saoBlurRadius = 50;
+                    saoPass.params.saoBlurStdDev = 5;
+                    saoPass.params.saoBlurDepthCutoff = 0.01;
+
+                    gl.setAnimationLoop(() => composer.render());
                 }}
             >
                 <MyScene numReindeers={numReindeers} />
