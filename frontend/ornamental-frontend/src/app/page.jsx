98b41a6774ae386d48f4a1d682c76a4fd05487ba
Xin Jun <cxj69.23@ichat.sp.edu.sg>
2024-12-18 15:24:26 +0800

    added UI on to Christmas tree page

--- frontend/ornamental-frontend/src/app/page.jsx
+++ frontend/ornamental-frontend/src/app/page.jsx
@@ -9,10 +9,6 @@ export default function Home() {
     const [numReindeers, setNumReindeers] = useState(0);
     return (
         <div className={css.scene}>
-            <div className={css.overlay}>
-                <input type="input" onChange={(e) => setNumReindeers(e.target.value)} />
-                <p>I am above</p>
-            </div>
             <Canvas
                 ref={cam}
                 shadows
@@ -24,6 +20,25 @@ export default function Home() {
             >
                 <MyScene numReindeers={numReindeers} />
             </Canvas>
+
+            <div className={css.overlay}>
+                <div className={css.container}>
+                    <div className={css.timer}>10 days 10 hours 10 minutes 10 seconds</div>
+                </div>
+
+                <div id="admin-panel">
+                    <strong>ADMIN</strong>
+                    <button>Start Secret Santa</button>
+                    <button>Start Next Activity</button>
+                </div>
+
+                <div className={css.giftbutton}>
+                    <button>
+                        <img src="/Gift.png" alt="Gift!" />
+                        Gift!
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
