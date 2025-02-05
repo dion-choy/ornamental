3898aea6a031b618a1a59fa4a292186dde94879e
dion-choy <dionchoynj@gmail.com>
2025-02-06 01:15:04 +0800

    working christmas star again cause someone removed it

--- src/src/app/test/page.jsx
+++ src/src/app/test/page.jsx
@@ -2,23 +2,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import { redirect } from "next/navigation";
 import "@/styles/Home.css";
-import {
-  checkCode,
-  getQuestions,
-  hasSecretSantaStarted,
-} from "@/components/api/api";
+import { checkCode, getQuestions, hasSecretSantaStarted } from "@/components/api/api";
 import { motion } from "motion/react";
 
 function test() {
-  async function checksanta() {
-    console.log(await hasSecretSantaStarted(41934));
-    console.log(await getQuestions(0000));
-  }
-  return (
-    <>
-      <button onClick={checksanta}>check santa</button>
-    </>
-  );
+    async function checksanta() {
+        console.log(await hasSecretSantaStarted(41934));
+        console.log(await getQuestions(0000));
+    }
+    return (
+        <>
+            <button onClick={checksanta}>check santa</button>
+        </>
+    );
 }
 
 export default test;
