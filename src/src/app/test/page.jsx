88088d59fcd30824d755c37f98004f94e6533c00
jer123se12 <jerickseng06@gmail.com>
2025-02-06 01:27:53 +0800

    updated api

--- src/src/app/test/page.jsx
+++ src/src/app/test/page.jsx
@@ -1,20 +1,22 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { redirect } from "next/navigation";
+import { useCookies, CookiesProvider } from "next-client-cookies";
 import "@/styles/Home.css";
 import { checkCode, getQuestions, hasSecretSantaStarted } from "@/components/api/api";
 import { motion } from "motion/react";
 
 function test() {
-    async function checksanta() {
-        console.log(await hasSecretSantaStarted(41934));
-        console.log(await getQuestions(0000));
-    }
-    return (
-        <>
-            <button onClick={checksanta}>check santa</button>
-        </>
-    );
+  const cookie=useCookies();
+  async function checksanta() {
+    console.log(await hasSecretSantaStarted(41934));
+    console.log(await getQuestions("0000", cookie.get("userId")));
+  }
+  return (
+    <>
+      <button onClick={checksanta}>check santa</button>
+    </>
+  );
 }
 
 export default test;
